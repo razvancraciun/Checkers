4d4a632fd8ea5981cef1b80088c5becb5847e7bf
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2019-10-27 16:37:55 +0200

    implemented game loop

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python3.7
+
 import game as g
+from os import system, name
+
+def clear():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
 
 def main():
     state = g.initialize()
-    g.display(state)
+    while not g.is_final_state(state):
+        clear()
+        g.display(state)
+        
+        if state[1] == 1:
+            old_pos, new_pos = (0, 0), (0, 0)
+            try:
+                print('choose a piece to move:')
+                old_pos = (int(input('   row = ')), int(input('   col = ')))
+                print('choose where to move it:')
+                new_pos = (int(input('   row = ')), int(input('   col = ')))
+            except:
+                continue
+            if g.is_valid_transition(state, old_pos, new_pos):
+                state = g.transition(state, old_pos, new_pos)
+        else:
+            # lab 5 stuff goes here
+            pass
 
 if __name__ == '__main__':
     main()
