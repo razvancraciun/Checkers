7d691831772ea952397f008787bd39da6cb4de53
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2019-10-30 19:44:16 +0200

    refactoring + 2 player

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3.7
 
-import game as g
 from os import system, name
+from termcolor import colored
+import game as g
 
 def clear():
     if name == 'nt':
@@ -9,24 +10,51 @@ def clear():
     else:
         system('clear')
 
+INPUT_ERROR_MSG = colored('Input is invalid!', 'red') + ' Try again.'
+MOVE_ERROR_MSG = colored('Move is invalid!', 'red') + ' Try another move.'
+
 def main():
-    state = g.initialize()
+    state, msg = g.initialize(), ''
     while not g.is_final_state(state):
-        # clear()
-        g.display(state)
-        if state[1] == 1:
-            old_pos, new_pos = (0, 0), (0, 0)
-            try:
-                print('choose a piece to move:')
-                old_pos = (int(input('   row = ')), int(input('   col = ')))
-                print('choose where to move it:')
-                new_pos = (int(input('   row = ')), int(input('   col = ')))
-            except:
-                continue
-            if g.is_valid_transition(state, old_pos, new_pos) and g.transition(state, old_pos, new_pos) in g.possible_transitions(state):
-                state = g.transition(state, old_pos, new_pos)
+        # display
+        clear()
+        print(msg)
+        msg = ''
+        g.display_board(state)
+
+        # game logic
+        old_pos, new_pos = (0, 0), (0, 0)
+        try:
+            print('choose a piece to move:')
+            old_pos = (int(input('   row = ')), int(input('   col = ')))
+            print('choose where to move it:')
+            new_pos = (int(input('   row = ')), int(input('   col = ')))
+        except:
+            msg = INPUT_ERROR_MSG
+            continue
+        if g.is_valid_transition(state, old_pos, new_pos):
+            state = g.transition(state, old_pos, new_pos)
         else:
-            pass
+            msg = MOVE_ERROR_MSG
 
 if __name__ == '__main__':
     main()
+
+
+# if state[1] == 1:
+#     old_pos, new_pos = (0, 0), (0, 0)
+#     try:
+#         print('choose a piece to move:')
+#         old_pos = (int(input('   row = ')), int(input('   col = ')))
+#         print('choose where to move it:')
+#         new_pos = (int(input('   row = ')), int(input('   col = ')))
+#     except:
+#         msg = INPUT_ERROR_MSG
+#         continue
+#     if g.is_valid_transition(state, old_pos, new_pos):
+#         state = g.transition(state, old_pos, new_pos)
+#     else:
+#         msg = MOVE_ERROR_MSG
+# else:
+#    # computer logic here
+#    pass
