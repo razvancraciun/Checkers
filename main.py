d732d4d2949316b19a811ff62cc234bba315a834
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2019-10-31 09:51:02 +0200

    implemented computer opponent logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3.7
 
 from os import system, name
+from random import randint
+from time import sleep
 from termcolor import colored
 import game as g
 
@@ -13,6 +15,8 @@ def clear():
 INPUT_ERROR_MSG = colored('Input is invalid!', 'red') + ' Try again.'
 MOVE_ERROR_MSG = colored('Move is invalid!', 'red') + ' Try another move.'
 
+# TODO: actual computer login (choose transition based on heuristic)
+# display winner
 def main():
     state, msg = g.initialize(), ''
     while not g.is_final_state(state):
@@ -21,40 +25,29 @@ def main():
         print(msg)
         msg = ''
         g.display_board(state)
-
+        if state[1] == -1:
+            sleep(1)
         # game logic
-        old_pos, new_pos = (0, 0), (0, 0)
-        try:
-            print('choose a piece to move:')
-            old_pos = (int(input('   row = ')), int(input('   col = ')))
-            print('choose where to move it:')
-            new_pos = (int(input('   row = ')), int(input('   col = ')))
-        except:
-            msg = INPUT_ERROR_MSG
-            continue
-        if g.is_valid_transition(state, old_pos, new_pos):
-            state = g.transition(state, old_pos, new_pos)
+        if state[1] == 1:
+            old_pos, new_pos = (0, 0), (0, 0)
+            try:
+                print('choose a piece to move:')
+                old_pos = (int(input('   row = ')), int(input('   col = ')))
+                print('choose where to move it:')
+                new_pos = (int(input('   row = ')), int(input('   col = ')))
+            except:
+                msg = INPUT_ERROR_MSG
+                continue
+            if g.is_valid_transition(state, old_pos, new_pos):
+                state = g.transition(state, old_pos, new_pos)
+            else:
+                msg = MOVE_ERROR_MSG
         else:
-            msg = MOVE_ERROR_MSG
+            possible_states = [g.transition(state, t[0], t[1]) for t in g.possible_transitions(state)]
+            min_heuristic = min([g.heuristic(s) for s in possible_states])
+            possible_states = list(filter(lambda s: g.heuristic(s) == min_heuristic, possible_states))
+            
+            state = possible_states[randint(0, len(possible_states) - 1)]
 
 if __name__ == '__main__':
     main()
-
-
-# if state[1] == 1:
-#     old_pos, new_pos = (0, 0), (0, 0)
-#     try:
-#         print('choose a piece to move:')
-#         old_pos = (int(input('   row = ')), int(input('   col = ')))
-#         print('choose where to move it:')
-#         new_pos = (int(input('   row = ')), int(input('   col = ')))
-#     except:
-#         msg = INPUT_ERROR_MSG
-#         continue
-#     if g.is_valid_transition(state, old_pos, new_pos):
-#         state = g.transition(state, old_pos, new_pos)
-#     else:
-#         msg = MOVE_ERROR_MSG
-# else:
-#    # computer logic here
-#    pass
