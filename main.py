684257bf928352dac0280ed1487616826b567617
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2019-11-07 09:58:07 +0200

    done

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
 #!/usr/bin/python3.7
 
-from os import system, name
-from random import randint
-from time import sleep
-from termcolor import colored
 import game as g
 import strategy as s
-import time
+from os import system, name
+from time import sleep
+from termcolor import colored
 
 def clear():
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
+    if name == 'nt': system('cls')
+    else: system('clear')
 
 INPUT_ERROR_MSG = colored('Input is invalid!', 'red') + ' Try again.'
 MOVE_ERROR_MSG = colored('Move is invalid!', 'red') + ' Try another move.'
-
-log = open('log.txt', 'w')
 
 def main():
     state, msg = g.initialize(), ''
     while g.is_final_state(state) == 0:
         # display
         clear()
-        print(msg)
-        msg = ''
+        print(msg) ; msg = ''
         g.display_board(state)
-        # if state[1] == -1:
-        #     sleep(1)
+        if state[1] == -1: sleep(0.6)
         # game logic
         if state[1] == 1:
-            # old_pos, new_pos = (0, 0), (0, 0)
-            # try:
-            #     print('choose a piece to move:')
-            #     old_pos = (int(input('   row = ')), int(input('   col = ')))
-            #     print('choose where to move it:')
-            #     new_pos = (int(input('   row = ')), int(input('   col = ')))
-            # except:
-            #     msg = INPUT_ERROR_MSG
-            #     continue
-            # if g.is_valid_transition(state, old_pos, new_pos):
-            #     state = g.transition(state, old_pos, new_pos)
-            # else:
-            #     msg = MOVE_ERROR_MSG
-            starting_time = time.time()
-            t = s.maximum_value(state)[0]
-            state = g.transition(state, t[0], t[1])
-            log.write('White: Minimax ' + str( (time.time() - starting_time) * 1000 ) + '\n')
+            old_pos, new_pos = (0, 0), (0, 0)
+            try:
+                print('choose a piece to move:')
+                old_pos = (int(input('   row = ')), int(input('   col = ')))
+                print('choose where to move it:')
+                new_pos = (int(input('   row = ')), int(input('   col = ')))
+            except:
+                msg = INPUT_ERROR_MSG
+                continue
+            if g.is_valid_transition(state, old_pos, new_pos):
+                state = g.transition(state, old_pos, new_pos)
+            else:
+                msg = MOVE_ERROR_MSG
         else:
-            starting_time = time.time()
             t = s.minimum_value(state)[0]
             state = g.transition(state, t[0], t[1])
-            log.write('Black: Minimax ' + str( (time.time() - starting_time) * 1000 ) + '\n')
 
     # end game
     clear()
